6fc353d9fd3183a150a913e81a6f9f88b8666894
Robert P <robisys@users.noreply.github.com>
2017-03-05 17:12:39 +0100

    Update compose-start.sh

--- mybsp/compose-start.sh
+++ mybsp/compose-start.sh
@@ -4,3 +4,7 @@ docker-compose up  -d
 #docker-compose logs             # ctrl-c beendet die Loganzeige
 #docker-compose logs rocketchat  # Ausgabe nur eines Containers
 #docker-compose down             # Container stoppen UND löschen
+
+# docker ps -a -q |xargs docker stop
+# docker ps -q | xargs docker rm
+
