706707f5ec8fad99650fa30134dcf1acc9a9bd8a
Robert P <robisys@users.noreply.github.com>
2017-03-07 17:05:33 +0100

    Add files via upload

--- mybsp/public/index.php
+++ mybsp/public/index.php
@@ -1,1 +1,48 @@
-#
+<html>
+<body>
+<h1> Start </h1>
+
+
+
+<p><a href="phpinfo.php"> phpinfo </a>
+</p>
+
+<p><a href="connect.php"> connect </a>
+</p>
+
+<p><a href="http://localhost:8081"> phpadmin </a>
+</p>
+
+<p><a href="/newdb_root.php"> newdb_root </a>
+</p>
+
+<p><a href="/createdb.php"> createdb </a>
+</p>
+
+<p><a href="/contao/contao/install.php"> contao install </a>
+</p>
+
+<p><a href="http://localhost:8080/contao/contao/index.php"> contao configurieren </a>
+</p>
+
+<p><a href="http://localhost:8080/contao/index.php"> contao  </a>
+</p>
+
+
+<p><a href="/contao-check/check/index.php"> contao check </a>
+</p>
+
+
+<p><a href="/contao/check/index.php"> my_contao  innerhalb check </a>
+</p>
+
+
+<p><a href="/my_contao-check/check/index.php"> my_contao check </a>
+</p>
+
+
+
+
+
+</body>
+<html>
---
+++ mybsp/public/phpinfo.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1> Start </h1>
+
+
+<?php
+
+phpinfo();
+?>
+
+</body>
+<html>
