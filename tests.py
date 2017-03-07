4f2f0cb454123d8e14e2c14a3ebf640ea7d2f0de
Robert P <robisys@users.noreply.github.com>
2017-03-07 20:08:36 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,20 +6,23 @@ import sys
     
 def run_unittests():
     ''' Execute Unit Tests '''
-    ### tests = unittest.TestLoader().discover('tests/unit')
-    ### result = unittest.TextTestRunner(verbosity=2).run(tests)
+    ###tests = unittest.TestLoader().discover('tests/unit')
+    tests = unittest.TestLoader().discover('tests/unit')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
     return result.wasSuccessful()
 
 def run_integration_tests():
     ''' Execute Integration Tests '''
-    ### tests = unittest.TestLoader().discover('tests/integration')
-    ### result = unittest.TextTestRunner(verbosity=2).run(tests)
+    ###tests = unittest.TestLoader().discover('tests/integration')
+    tests = unittest.TestLoader().discover('tests/unit')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
     return result.wasSuccessful()
 
 def run_functional_tests():
     ''' Execute Functional Tests '''
-    ### tests = unittest.TestLoader().discover('tests/functional')
-    ### result = unittest.TextTestRunner(verbosity=2).run(tests)
+    ###tests = unittest.TestLoader().discover('tests/functional')
+    tests = unittest.TestLoader().discover('tests/unit')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
     return result.wasSuccessful()
 
 if __name__ == '__main__':
