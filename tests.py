49bdb0cfc4358e19252fb294a2efb0cbeb577a1e
Robert P <robisys@users.noreply.github.com>
2017-03-08 11:36:21 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,48 +2,8 @@
 
 ### import unittest
 import sys
- sys.exit(0)
-    
-def run_unittests():
-    ''' Execute Unit Tests '''
-    ###tests = unittest.TestLoader().discover('tests/unit')
-    tests = true
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    return result.wasSuccessful()
 
-def run_integration_tests():
-    ''' Execute Integration Tests '''
-    ###tests = unittest.TestLoader().discover('tests/integration')
-    tests = true
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    return result.wasSuccessful()
-
-def run_functional_tests():
-    ''' Execute Functional Tests '''
-    ###tests = unittest.TestLoader().discover('tests/functional')
-    tests = true
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    return result.wasSuccessful()
-
-if __name__ == '__main__':
-    print "#" * 70
-    print "Test Runner: Unit tests"
-    print "#" * 70
-    unit_results = run_unittests()
-
-    print "#" * 70
-    print "Test Runner: Integration tests"
-    print "#" * 70
-    integration_results = run_integration_tests()
-
-    print "#" * 70
-    print "Test Runner: Functional tests"
-    print "#" * 70
-    functional_results = run_functional_tests()
-
-    if unit_results and integration_results 
-         and functional_results:
+if  x =0 :
         sys.exit(0)
     else:
-        sys.exit(1)
-      
+sys.exit(1)    
